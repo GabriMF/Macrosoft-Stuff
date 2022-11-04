c6f6ea6214e80feed31b9a9dac7141ac7e27eec4
David Daza <drdaza18@gmail.com>
2022-11-04 22:29:05 +0100

    nothing importante

--- script.js
+++
@@ -1,5 +0,0 @@
-let btn=document.querySelector(".btn");
-let body=document.querySelector("body");
-btn.onclick = function(){
-    body.classList.toggle("on");
-}
---
+++ src/script.js
@@ -0,0 +1,5 @@
+var btn=document.querySelector(".btn");
+var body=document.querySelector("body");
+btn.onclick = function(){
+    body.classList.toggle("on");
+}
